bee85da9253c0d9026f9924e935ca6011ede1f03
Alexander Schnaidt <alex.schnaidt@gmail.com>
2013-01-07 23:06:52 +0100

    clean up and add basic .gitignore

--- weespeak.py
+++ weespeak.py
@@ -1,19 +1,17 @@
-#
-#
-#
+# -*- coding: utf-8 -*-
 
 SCRIPT_NAME    = "weespeak"
-SCRIPT_AUTHOR  = "alex.schnaidt@gmail.com"
+SCRIPT_AUTHOR  = "Alexander Schnaidt <alex.schnaidt@gmail.com>"
 SCRIPT_VERSION = "0.1"
 SCRIPT_LICENSE = "GPL2"
-SCRIPT_DESC    = "Outputs messages in current buffer through espeak."
+SCRIPT_DESC    = "Outputs messages in current irc-buffer through espeak."
 SCRIPT_COMMAND = "weespeak"
 
 import sys
 
 try:
     import weechat
-    from weechat import WEECHAT_RC_OK
+    from weechat import WEECHAT_RC_OK as W_OK
 except ImportError:
     raise ImportError("This script has to be run from inside weechat: '/python load /path/to/script/weespeak.py'")
 
@@ -24,8 +22,8 @@ except ImportError:
     weechat.prnt("", "This script requires python-espeak: https://launchpad.net/python-espeak")
     raise
 
-syntax="{who} says {what}"
-ignore=["phrik"]
+template = "{who} says {what}"
+ignore = []
 max_message_length = 500
 
 def own_nick(server):
@@ -52,28 +50,31 @@ def parse_message(data, signal, signal_data):
     return hm
 
 def speak_out(data, signal, signal_data):
-    """ """
+
     msg = parse_message(data, signal, signal_data)
 
-    WOK = WEECHAT_RC_OK # WTF?
+    W_OK = WEECHAT_RC_OK
 
     # filter out message that would take to long to synthesize by espeak
     if len(msg["message"]) > max_message_length:
         return WOK
 
-    # you don't want to hear your own messages?
+    # you don't want to hear your own messages
     elif msg["nick"] == own_nick(msg["server"]):
-        return WOK
+        return WEECHAT_RC_OK
+
     # don't speak out if nick is in ignore list
     elif msg["nick"] in ignore:
-        return WOK
+        return WEECHAT_RC_OK
+
     # only speak out the current buffer
     elif buffer_current() != buffer(msg["server"], msg["channel"]):
-        return WOK
+        return WEECHAT_RC_OK
+
     # build sentence
     sentence = syntax.format(who = msg["nick"], what = msg["message"])
 
-    # send to espeak
+    # run it through espeak
     espeak.synth(sentence)
 
     return WOK
@@ -81,10 +82,14 @@ def speak_out(data, signal, signal_data):
 # weechat.register(name, author, version, 
 #                  license, description, shutdown_function, charset)
 weechat.register(SCRIPT_NAME,
-                SCRIPT_AUTHOR,
-                SCRIPT_VERSION,
-                SCRIPT_LICENSE,
-                SCRIPT_DESC, "", "")
+                 SCRIPT_AUTHOR,
+                 SCRIPT_VERSION,
+                 SCRIPT_LICENSE,
+                 SCRIPT_DESC, "", "")
 
-# run speak_out() AFTER the message got processed => irc_in2_privmsg; run before => irc_in_privmsg
+# hook into weechat:
+
+# run speak_out() AFTER the message got processed => irc_in2_privmsg;
+# run BEFORE => irc_in_privmsg
 weechat.hook_signal("*,irc_in2_privmsg", "speak_out", "")
+
